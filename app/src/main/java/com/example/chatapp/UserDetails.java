7f79d5aac0b2f374109651e35561ce760ca1fafe
Nameofthegame <46572186+Nameofthegame@users.noreply.github.com>
2020-11-28 15:31:52 -0500

    Create UserDetails.java

---
+++ app/src/main/java/com/example/chatapp/UserDetails.java
@@ -0,0 +1,9 @@
+package com.example.chatapp;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String name = "";
+    static String email = "";
+    static String chatWith = "";
+}
